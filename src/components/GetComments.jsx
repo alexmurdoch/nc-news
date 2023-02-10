bc6cad2676172cfd2a7e17537dd49ba762c4872b
alexmurdoch <almurdoch62@gmail.com>
2023-02-10 16:38:58 +0000

    "done"

--- src/components/GetComments.jsx
+++ src/components/GetComments.jsx
@@ -1,37 +1,49 @@
-import {getCommentsByArticleId} from "../api"
+import { getCommentsByArticleId } from "../api";
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { AddComment } from "./AddComment";
 import { DeleteComment } from "./DeleteComment";
 
 export const GetComments = () => {
-  
-    const { article_id } = useParams();
-    const [comments, setComments] = useState([]);
-    useEffect(() => {
-        getCommentsByArticleId(article_id).then((result) => {
-          setComments(result);
-        });
-      }, []);
-    return (
+  const { article_id } = useParams();
+  const [comments, setComments] = useState([]);
+  useEffect(() => {
+    getCommentsByArticleId(article_id).then((result) => {
+      setComments(result);
+    })
+    .catch((error) => {
+      console.error("Error while posting comment:", error);
+    });
+  }, []);
+  return (
     <ul>
-        <AddComment articleId={article_id} setComments={setComments} />
+      <AddComment
+        articleId={article_id}
+        setComments={setComments}
+        comments={comments}
+      />
 
-        {comments.map((comment) => {
-            return (
+      {comments.map((comment) => {
+        return (
+          <section key={comment.comment_id}>
+            {comment.author === "grumpy19" && (
+              <DeleteComment
+                commentId={comment.comment_id}
+                setComments={setComments}
+                comments={comments}
+              />
+            )}
 
-            <section key = {comment.comment_id}>
-              {comment.author === "grumpy19" && <DeleteComment commentId={comment.comment_id} setComments={setComments} comments={comments}/>}
-             
-              <p className="commentBody" >
-                {comment.body}
-              </p>
-              <p className="commentAuthor">{comment.author}</p>
-              <p className="commentVotes">{comment.votes}</p>
-              <p className="createdAt">{comment.created_at.slice(0, 10)}</p>
-            </section>
+            <p className="commentBody">{comment.body}</p>
+            <p className="commentAuthor">{comment.author}</p>
+            <p className="commentVotes">{comment.votes}</p>
+            <p className="createdAt">
             
-          );
-        })
-}</ul>)
-}
+              {comment.created_at.slice(0, 10)}
+            </p>
+          </section>
+        );
+      })}
+    </ul>
+  );
+};
